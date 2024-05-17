3734359a88295a85fdfb4fa3a437fd23ba65589a
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2024-05-18 01:46:20 +0300

    created options for menu

--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -28,8 +28,49 @@ namespace LibraryApp
             Console.WriteLine();
             Console.WriteLine("*************************************************************************************");
             Console.WriteLine();
+            Console.WriteLine("***** İşlemler Menüsü  *****");
+            Console.WriteLine();
+            Console.WriteLine("1-Fiyat Sorgulama");
+            Console.WriteLine("2-Yeni Okur Kaydı");
+            Console.WriteLine("3-Günün Kitabı");
+            Console.WriteLine("4-Kitap Arşivi");
+            Console.WriteLine("5-Yeni Kitap Al");
+            Console.WriteLine("6-Oyun");
+            Console.WriteLine();
+            Console.Write("Yapmak istediğiniz işlemin numarasınız yazınız: ");
+            char option = Convert.ToChar(Console.ReadLine());
+            if (option.Equals('1'))
+            {
+                Console.WriteLine();
+                Console.WriteLine("***** Fiyat Sorgulama  *****");
+            }
+            if (option.Equals('2'))
+            {
+                Console.WriteLine();
+                Console.WriteLine("***** Yeni Okur Kaydı  *****");
+            }
+            if (option.Equals('3'))
+            {
+                Console.WriteLine();
+                Console.WriteLine("***** Günün Kitabı  *****");
+            }
+            if (option.Equals('4'))
+            {
+                Console.WriteLine();
+                Console.WriteLine("***** Kitap Arşivi  *****");
+            }
+            if (option.Equals('5'))
+            {
+                Console.WriteLine();
+                Console.WriteLine("***** Yeni Kitap Al  *****");
+            }
+            if (option.Equals('6'))
+            {
+                Console.WriteLine();
+                Console.WriteLine("***** Oyun  *****");
+            }
 
-           
+
 
             Console.ReadKey();
         }
