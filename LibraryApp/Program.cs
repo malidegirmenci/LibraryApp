60adabf670084c49d65eddd3154eb094de6c5de8
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2024-05-18 01:37:20 +0300

    created list of books

--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -10,10 +10,26 @@ namespace LibraryApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*******************************************************************");
+            Console.WriteLine("*************************************************************************************");
             Console.WriteLine();
-            Console.WriteLine("**  Türkçe Kitaplar Kategorisi  **  Yabancı Kitaplar Kategorisi  **");
+            Console.WriteLine("**  Türkçe Kitaplar Kategorisi             **  Yabancı Kitaplar Kategorisi         **");
             Console.WriteLine();
+            Console.WriteLine("**  1-Çalıkuşu: Reşat Nuri                 **  7-Tuna Kılavuzu: Jules Verne        **");
+            Console.WriteLine();
+            Console.WriteLine("**  2-Yaban: Yakup Kadri                   **  8-Bir Kuzey Macerası: Jack London   **");
+            Console.WriteLine();
+            Console.WriteLine("**  3-Sinekli Bakkal: Halide Edip          **  9-Altıncı Koğuş: Anton Çehov        **");
+            Console.WriteLine();
+            Console.WriteLine("**  4-Tehlikeli Oyunlar: Oğuz Atay         **  10-Kumarbaz: Dostoyevski            **");
+            Console.WriteLine();
+            Console.WriteLine("**  5-Geçtiğim Günlerden: Hasan Ali Yücel  **  11-Bir Kuzey Macerası: Jack London  **");
+            Console.WriteLine();
+            Console.WriteLine("**  6-Kuyucaklı Yusuf: Sabahattin Ali      **  12-Vişne Bahçesi: Anton Çehov       **");
+            Console.WriteLine();
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine();
+
+           
 
             Console.ReadKey();
         }
